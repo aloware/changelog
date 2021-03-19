4421ccd4854f5a55254eeeeb03abfb95ff17262d
Reggie Gamalo <reggie@aloware.com>
2021-03-20 05:58:15 +0800

    Fixed date alignment in widget.

--- resources/js/components/published-changelog-widget-list.vue
+++ resources/js/components/published-changelog-widget-list.vue
@@ -3,7 +3,7 @@
         <div class="callout-header">
             <p>
                 <span class="badge " v-bind:style="{backgroundColor: changelog.category.bg_color, color : changelog.category.text_color}">{{ changelog.category.label }}</span>
-                <small class="text-muted">
+                <small class="text-muted date-container">
                     <font-awesome-icon :icon="['far', 'clock']" />
                     <relative-time-component :from_time="changelog.created_at" :humanized="true" :update_interval="60000"></relative-time-component>
                 </small>
@@ -32,5 +32,9 @@ export default {
 
     .callout-header h4 {
         font-size: 1rem;
+    }
+
+    .date-container {
+        margin-top: 6px;
     }
 </style>
